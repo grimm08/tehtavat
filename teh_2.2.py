81a8665b53947dbbf3ef8441774b25c23b395bf3
grimm08 <akil.abdulrahim@gmail.com>
2026-08-18 13:43:16 +0300

    do

---
+++ teh_2.2.py
@@ -0,0 +1,4 @@
+import math
+sade = float(input("anna ympyrän säde: "))
+pinta = math.pi * (sade**2)
+print(f"ympyrän pinta-ala: {pinta:.2f}")
---
+++ teh_2.3.py
@@ -0,0 +1,9 @@
+import math
+h = float(input("anna korkeus: "))
+l = float(input("anna kanta: "))
+#piiri laskektus
+piiri  = 2*(l+h)
+#pinta-ala
+A = l*h
+
+print(f"piiri: {piiri:.2f}\npinta-ala: {A:.2f}")
